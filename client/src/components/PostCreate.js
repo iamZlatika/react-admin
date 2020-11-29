479d04f8a253301b2d03e3ffaa6f8889d159406b
iamZlatika <mazurovazv@gmail.com>
2020-11-29 16:36:00 +0300

    PostEdit Component

--- client/src/components/PostCreate.js
+++ client/src/components/PostCreate.js
@@ -5,6 +5,7 @@ const PostCreate = (props) => {
   return (
     <Create title='Create a Post' {...props}>
       <SimpleForm>
+      
         <TextInput source='title' />
         <TextInput multiline source='body' />
         <DateInput label='Published' source='publishedAt'/>
